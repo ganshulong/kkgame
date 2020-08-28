1ba0f17ee39913465ad5689a24d5d00ab1ab266e
ganshulong <153274152@qq.com>
2020-08-28 21:12:05 +0800

    红中 fix

--- assets/resources/hongzhong/script/HongZhong_RoundOver.js
+++ assets/resources/hongzhong/script/HongZhong_RoundOver.js
@@ -8,50 +8,19 @@ cc.Class({
         _show:false,
         _isOver:false,
         _OverScoreNode:null,
-        // _zhuang:-1,
     },
 
     start () {
         Global.registerEvent(EventId.HU_NOTIFY,this.recvRoundOverNotify,this);
         Global.registerEvent(EventId.CLEARDESK,this.clearDesk,this);
         Global.registerEvent(EventId.GAMEOVER,this.recvGameOver,this);
-        // Global.registerEvent(EventId.HANDCARD,this.recvHandCard,this);
-        // Global.registerEvent(EventId.GAME_RECONNECT_DESKINFO,this.recvDeskInfoMsg,this);
 
         this._OverScoreNode = cc.find("scene/over_score",this.node);
         this._OverScoreNode.active = false;
     },
 
-    // recvHandCard(data){
-    //   data = data.detail;
-    //   this._zhuang = data.bankerInfo.seat;
-
-    // },
-
-    // recvDeskInfoMsg(){
-    //     this._zhuang = cc.vv.gameData.getDeskInfo().bankerInfo.seat;
-    // },
-
     init(atlas){
         this._atlas = atlas;
-    },
-
-    createType(type, bInHandCards = false){
-        let node = new cc.Node();
-        let spr = node.addComponent(cc.Sprite);
-        let sprName = "";
-        if(type === cc.vv.gameData.OPERATETYPE.CHI) sprName = "penghu_onwer-table-imgs-wz_chi";
-        else if(type === cc.vv.gameData.OPERATETYPE.KAN && bInHandCards) sprName = "penghu_onwer-table-imgs-wz_kan";
-        else if(type === cc.vv.gameData.OPERATETYPE.KAN && !bInHandCards) sprName = "penghu_onwer-table-imgs-wz_sao";
-        else if(type === cc.vv.gameData.OPERATETYPE.PAO) sprName = "penghu_onwer-table-imgs-wz_pao";
-        else if(type === cc.vv.gameData.OPERATETYPE.LONG) sprName = "penghu_onwer-table-imgs-wz_tilong";
-        else if(type === cc.vv.gameData.OPERATETYPE.PENG) sprName = "penghu_onwer-table-imgs-wz_peng";
-        spr.spriteFrame = this._atlas.getSpriteFrame(sprName);
-        return node;
-    },
-
-    recvGameOver(){
-        this._isOver = true;
     },
 
     recvRoundOverNotify(data){
@@ -91,6 +60,24 @@ cc.Class({
 
     },
 
+    initRoomInfo(data){
+        let conf = cc.vv.gameData.getRoomConf();
+        let roomId = cc.find("roomInfoNode/txt_room_id",this._layer);
+        roomId.getComponent(cc.Label).string = "房间号:" + conf.deskId;
+
+        let roundNum = cc.find("roomInfoNode/txt_round_num",this._layer);
+        roundNum.getComponent(cc.Label).string = "局数:" + cc.vv.gameData.getDeskInfo().round + "/" + conf.gamenum;
+
+        let txt_date = cc.find("roomInfoNode/txt_date",this._layer);
+        txt_date.getComponent(cc.Label).string = data.overTime;
+
+        let txt_game_desc = cc.find("roomInfoNode/txt_game_desc",this._layer);
+        txt_game_desc.getComponent(cc.Label).string = cc.vv.gameData.getWanFaStrDetail();
+
+        let okBtn = this._layer.getChildByName("btn_comfirm");
+        Global.btnClickEvent(okBtn,this.onClose,this);
+    },
+
     showRoundInfo(data){
         this._layer.getChildByName("spr_draw").active = (0 > data.hupaiType && 0 == data.smallState);
         let panle_playerInfo = this._layer.getChildByName("panle_playerInfo");
@@ -105,7 +92,7 @@ cc.Class({
                 
                 let spr_head = cc.find("head/radio_mask/spr_head",player);
                 Global.setHead(spr_head, data.users[i].usericon);
-                player.getChildByName("spr_banker").active = (data.buck ==- data.users[i].uid);
+                player.getChildByName("spr_banker").active = (data.buck === data.users[i].uid);
                 player.getChildByName("text_name").getComponent(cc.Label).string = data.users[i].playername;
 
                 player.getChildByName("spr_huType").active = (data.seat === data.users[i].seat && 0 < data.hupaiType);
@@ -173,93 +160,8 @@ cc.Class({
         }
     },
 
-    initPlayerInfo(data){
-        let bigWinerScore = 0;
-        let bigWinerNode = null;
-        for(let i = 0; i < data.users.length; ++i){
-            let chairId = cc.vv.gameData.getLocalChair(data.users[i].seat);
-            let player = this._layer.getChildByName("player" + chairId);
-
-            let spr_head = cc.find("head/radio_mask/spr_head",player);
-            Global.setHead(spr_head, data.users[i].usericon);
-
-            player.getChildByName("spr_bigWiner").active = false;
-            player.getChildByName("spr_banker").active = (data.buck == data.users[i].uid);
-
-            player.getChildByName("text_name").getComponent(cc.Label).string = data.users[i].playername;
-            player.getChildByName("text_id").getComponent(cc.Label).string = "ID:"+data.users[i].uid;
-            if (0 <= data.users[i].roundScore) {
-                player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = ('/' + Math.abs(data.users[i].roundScore));
-                player.getChildByName("LabelAtlas_score_lose").getComponent(cc.Label).string = '';
-            } else {
-                player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = '';
-                player.getChildByName("LabelAtlas_score_lose").getComponent(cc.Label).string = ('/' + Math.abs(data.users[i].roundScore));
-            }
-            if (bigWinerScore < data.users[i].roundScore) {
-                bigWinerScore = data.users[i].roundScore;
-                bigWinerNode = player;
-            }
-        }
-        if (0 < bigWinerScore) {
-            bigWinerNode.getChildByName("spr_bigWiner").active = true;
-        }
-        for (let i = data.users.length; i < 4; i++) {
-            this._layer.getChildByName("player" + i).active = false;
-        }
-    },
-
-    // normal = 1, --平胡
-    // tianHu = 2, --天胡
-    // diHu = 3, --地胡
-    // tilongHu = 4, --踢龙胡
-    // paoHu = 5, --跑胡
-    // saoHu = 6, --扫胡
-    // pengHu = 7,--碰胡
-    // pengSanHu = 8, --碰三胡
-    // saoSanHu = 9, --扫胡
-    // pengSiHu = 10, --碰四胡
-    // saoSiHu = 11, --扫四胡
-    // shuangLongHu = 12, --双龙
-    // xiaoQiDuiHu = 13, --小七对
-    // paoDiHu = 14, --跑地胡
-    // pengDiHu = 15, --碰地胡
-    // pengDiHu = 16, --五福
-    showHuType(type,huFlag){
-        let sprName = "";
-        if(type===1) sprName = "penghu_onwer-table-imgs-huflag_12";
-        else if(type===2) sprName = "penghu_onwer-table-imgs-huflag_0";
-        else if(type===3 || type === 14 || type === 15) sprName = "penghu_onwer-table-imgs-huflag_1";
-        else if(type===4) sprName = "penghu_onwer-table-imgs-huflag_5";
-        else if(type===5) sprName = "penghu_onwer-table-imgs-huflag_6";
-        else if(type===6) sprName = "penghu_onwer-table-imgs-huflag_13";
-        else if(type===7) sprName = "penghu_onwer-table-imgs-huflag_9";
-        else if(type===8) sprName = "penghu_onwer-table-imgs-huflag_8";
-        else if(type===9) sprName = "penghu_onwer-table-imgs-huflag_13";
-        else if(type===10) sprName = "penghu_onwer-table-imgs-huflag_7";
-        else if(type===11) sprName = "penghu_onwer-table-imgs-huflag_11";
-        else if(type===12) sprName = "penghu_onwer-table-imgs-huflag_3";
-        else if(type===13) sprName = "penghu_onwer-table-imgs-huflag_2";
-        else if(type===16) sprName = "penghu_onwer-table-imgs-huflag_4";
-        huFlag.getComponent(cc.Sprite).spriteFrame = this._atlas.getSpriteFrame(sprName);
-    },
-
-
-    initRoomInfo(data){
-        let conf = cc.vv.gameData.getRoomConf();
-        let roomId = cc.find("roomInfoNode/txt_room_id",this._layer);
-        roomId.getComponent(cc.Label).string = "房间号:" + conf.deskId;
-
-        let roundNum = cc.find("roomInfoNode/txt_round_num",this._layer);
-        roundNum.getComponent(cc.Label).string = "局数:" + cc.vv.gameData.getDeskInfo().round + "/" + conf.gamenum;
-
-        let txt_date = cc.find("roomInfoNode/txt_date",this._layer);
-        txt_date.getComponent(cc.Label).string = data.overTime;
-
-        let txt_game_desc = cc.find("roomInfoNode/txt_game_desc",this._layer);
-        txt_game_desc.getComponent(cc.Label).string = cc.vv.gameData.getWanFaStrDetail();
-
-        let okBtn = this._layer.getChildByName("btn_comfirm");
-        Global.btnClickEvent(okBtn,this.onClose,this);
+    recvGameOver(){
+        this._isOver = true;
     },
 
     onClose(){
@@ -273,85 +175,6 @@ cc.Class({
         this._show = false;
         if(this._isOver) Global.dispatchEvent(EventId.SHOW_GAMEOVER);
         else Global.dispatchEvent(EventId.CLOSE_ROUNDVIEW);
-    },
-
-    initHandCard(list,parent,menzi,card=null,source=null){
-        let tempList = cc.vv.gameData.sortCard(list);
-        let width=0;
-        for(let i=0;i<tempList.length;++i){
-            for(let j=0;j<tempList[i].length;++j) {
-                let node = this.node.getComponent("HongZhong_Card").createCard(tempList[i][j],false);
-                node.scale = 0.5;
-                node.y = (node.height-28)*j*node.scale+20;
-                node.x = node.width*i*node.scale+20;
-                node.zIndex=4-j;
-                node.parent = parent;
-                if(j ===0){
-                    width = node.x+node.scale*node.width*0.5;
-                }
-            }
-        }
-
-        for(let i=0;i<menzi.length;++i){
-            let menzilist = [];
-            let typeData = menzi[i];
-            let typeNode = this.createType(typeData.type);
-            typeNode.parent = parent;
-            typeNode.zIndex = 1;
-            if(typeData.type === cc.vv.gameData.OPERATETYPE.KAN) // 坎
-            {
-                menzilist=[typeData.card,typeData.card,typeData.card];
-            }
-            else if(typeData.type === cc.vv.gameData.OPERATETYPE.LONG || typeData.type === cc.vv.gameData.OPERATETYPE.SHE||
-                typeData.type === cc.vv.gameData.OPERATETYPE.PAO){
-                menzilist=[typeData.card,typeData.card,typeData.card,typeData.card];
-            }
-            else if(typeData.type === cc.vv.gameData.OPERATETYPE.PENG){
-                menzilist=[typeData.card,typeData.card,typeData.card];
-            }
-            else if(typeData.type === cc.vv.gameData.OPERATETYPE.CHI){
-                menzilist=typeData.data;
-            }
-            let posX = 0;
-            let PosY = 0;
-            for(let j=0;j<menzilist.length;++j){
-                let node = this.node.getComponent("HongZhong_Card").createCard(menzilist[j],false);
-                node.scale = 0.45;
-                node.x = width+node.width*node.scale;
-                node.y = 44;
-                node.parent = parent;
-                width += (node.width*node.scale);
-
-                if(j===1){
-                    PosY = node.height*node.scale*0.5+40;
-                    if(menzilist.length===3) posX = node.x;
-                    else if(menzilist.length===4){
-                        posX = node.x+node.width*node.scale;
-                    }
-                    else if(menzilist.length===2){
-                        posX = node.x-node.width*node.scale;
-                    }
-                }
-            }
-            typeNode.x = posX;
-            typeNode.y = PosY;
-
-            width += 10;
-
-        }
-
-        if(card){
-            let bgNode = new cc.Node();
-            bgNode.addComponent(cc.Sprite);
-            this.node.getComponent("HongZhong_Card").changCardBg(bgNode,source===0);
-            let cardNode = this.node.getComponent("HongZhong_Card").createCard(card,false);
-            bgNode.y = 43;
-            bgNode.x = width+60;
-            bgNode.scale = 0.6;
-
-            cardNode.parent = bgNode;
-            bgNode.parent = parent;
-        }
     },
 
     clearDesk(){
@@ -369,5 +192,4 @@ cc.Class({
             cc.loader.releaseRes("common/prefab/HongZhong_round_over_view",cc.Prefab);
         }
     }
-    // update (dt) {},
 });
