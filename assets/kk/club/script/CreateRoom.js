69c51b4e29a96c7f2d4fa13240be20e45edc5b03
ganshulong <153274152@qq.com>
2020-11-10 10:25:53 +0800

    同花倍数 0.1-10

--- assets/kk/club/script/CreateRoom.js
+++ assets/kk/club/script/CreateRoom.js
@@ -214,7 +214,7 @@ cc.Class({
         let text_score = cc.find("right_bg/scrollview/content/bg_score/text_score",this.gamePanels[this.curGameIndex]);
         let score = Number(text_score.getComponent(cc.Label).string);
         let changeValue = event.target.isTongHua ? 0.1 : 1;
-        if (changeValue * 10 > score) {
+        if (10 > score) {
             score += changeValue;
             if (event.target.isTongHua) {
                 score = score.toFixed(1);
