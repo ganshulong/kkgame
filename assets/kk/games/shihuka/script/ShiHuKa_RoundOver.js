984449930556f7f54d9be395ae2a3063337a1b97
ganshulong <153274152@qq.com>
2020-11-18 11:34:29 +0800

    字牌剩余牌显示优化

--- assets/kk/games/shihuka/script/ShiHuKa_RoundOver.js
+++ assets/kk/games/shihuka/script/ShiHuKa_RoundOver.js
@@ -224,9 +224,11 @@ cc.Class({
             panel_CardInfo.getChildByName("text_tun_fan").getComponent(cc.Label).string = tunFanStr;
         }
         let surplusCard = this._layer.getChildByName("surplusCard");
+        let rowCardNum = 29;
         for (let i = 0; i < data.diPai.length; i++) {
             let node = this.node.getComponent("ShiHuKa_Card").createCard(data.diPai[i],2);
-            node.x = (node.width + 5) * i;
+            node.scale = 0.78;
+            node.x = (node.width * node.scale + 3) * (i % rowCardNum);
             node.parent = surplusCard;
         }
     },
