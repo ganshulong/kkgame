0f958e11b289b043c09dd57c32f1be985be22949
ganshulong <153274152@qq.com>
2020-12-09 14:54:48 +0800

    去掉大厅预加载创建房间页面

--- assets/kk/script/Lobby.js
+++ assets/kk/script/Lobby.js
@@ -128,7 +128,7 @@ cc.Class({
         this.initJoinGame();
 
         this.CreateRoomJS = this.node.getComponent("CreateRoom");
-        this.CreateRoomJS.preLoadPrefab(false);
+        // this.CreateRoomJS.preLoadPrefab(false);
         
         this.node.addComponent("HallRecord");
         this.HallRecordJS = this.node.getComponent("HallRecord");
