4d9b4fabd4f4f833e9ae4458dbda7be7d014c9d6
ganshulong <153274152@qq.com>
2020-12-30 10:58:00 +0800

    +继续下一局功能 红中

--- assets/kk/games/hongzhong/script/HongZhong_Player.js
+++ assets/kk/games/hongzhong/script/HongZhong_Player.js
@@ -79,6 +79,11 @@ cc.Class({
         Global.registerEvent(EventId.UPDATE_PLAYER_INFO,this.onRcvUpdatePlayerInfo,this);
         Global.registerEvent(EventId.TRUSTEE_NOTIFY,this.onRcvTrusteeNotify,this);
         Global.registerEvent(EventId.CANCEL_TRUSTEE_NOTIFY,this.onRcvCancelTrusteeNotify,this);
+        Global.registerEvent(EventId.GAME_CONTINUE_NOTIFY,this.onRcvGameContinueNotify,this);
+    },
+
+    onRcvGameContinueNotify(){
+        this.setTotalScore(0);
     },
 
     onRcvCancelTrusteeNotify(data){
@@ -393,7 +398,19 @@ cc.Class({
             let chairId = cc.vv.gameData.getLocalChair(msg.seat);
             if(chairId === this._chairId){
                 this._seatIndex = -1;
-                if(this._playerNode) this._playerNode.active = false;
+                if(this._playerNode) {
+                    this._playerNode.active = false;
+
+                    let uiSeat = cc.vv.gameData.getUISeatBylocalSeat(this._chairId);
+                    let out_cards = this._playerNode.parent.getChildByName("out_cards");
+                    out_cards.getChildByName("out_card"+uiSeat).removeAllChildren();
+                    out_cards.getChildByName("curOutCardTipsPos"+uiSeat).active = false;
+                    
+                    if (uiSeat) {
+                        let playback_handle = this._playerNode.parent.getChildByName("hand_cards");
+                        playback_handle.getChildByName("hand_card"+uiSeat).removeAllChildren();
+                    }
+                }
             }
         }
     },
