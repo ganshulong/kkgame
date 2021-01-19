612f17f7c425fca9fc199e8b6895a5656ada73c9
ganshulong <153274152@qq.com>
2021-01-19 17:13:48 +0800

    热更1.2.34

--- version_generator.js
+++ version_generator.js
@@ -12,7 +12,7 @@ var manifest = {
     packageUrl: 'http://123.57.94.118/remote-assets/',
     remoteManifestUrl: 'http://123.57.94.118/remote-assets/project.manifest',
     remoteVersionUrl: 'http://123.57.94.118/remote-assets/version.manifest',
-    version: '1.2.33',
+    version: '1.2.34',
     assets: {},
     searchPaths: []
 };
