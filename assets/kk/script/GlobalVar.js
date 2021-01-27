1a333a52956fc9baa4c988d04b84ccf984cf71df
ganshulong <153274152@qq.com>
2021-01-27 17:45:01 +0800

    1.2.44 热更

--- assets/kk/script/GlobalVar.js
+++ assets/kk/script/GlobalVar.js
@@ -13,7 +13,7 @@ var GlobalVar = cc.Class({
   		// 登录游戏服地址
         // loginServerAddress:"47.241.145.84:7180",         //开发测试 106.12.7.114:7180
         loginServerAddress:"www.kayuxin.com:7180",      //正式 8.131.94.204:7180  www.kayuxin.com
-        resVersion:"1.2.43",
+        resVersion:"1.2.44",
         
         //非常用的配置
         localVersion:true,
