b157e672a18c75d965dcfb39324c083ecd84952f
ganshulong <153274152@qq.com>
2021-01-28 11:26:50 +0800

    1.2.45 热更

--- version_generator.js
+++ version_generator.js
@@ -12,7 +12,7 @@ var manifest = {
     packageUrl: 'http://www.kayuxin.com/remote-assets/',
     remoteManifestUrl: 'http://www.kayuxin.com/remote-assets/project.manifest',
     remoteVersionUrl: 'http://www.kayuxin.com/remote-assets/version.manifest',
-    version: '1.2.44',
+    version: '1.2.45',
     assets: {},
     searchPaths: []
 };
