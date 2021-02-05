acf5dc0128d010e5bd2c2fc0b92820fa6d632a68
ganshulong <153274152@qq.com>
2021-02-05 15:42:20 +0800

    跑得快大小结算位数显示

--- assets/kk/games/paodekuai/script/PaoDeKuai_GameOver.js
+++ assets/kk/games/paodekuai/script/PaoDeKuai_GameOver.js
@@ -93,7 +93,7 @@ cc.Class({
             let icon = cc.find("radio_mask/spr_head",head);
             Global.setHead(icon,user.usericon);
 
-            player.getChildByName("mask_master").active = (user.uid == createID);
+            // player.getChildByName("mask_master").active = (user.uid == createID);
 
             player.getChildByName("txt_name").getComponent(cc.Label).string = user.playername;
             player.getChildByName("txt_id").getComponent(cc.Label).string = "ID:"+user.uid;
@@ -108,12 +108,16 @@ cc.Class({
             let bg_bar = player.getChildByName("bg_bar");
             if (0 <= user.scoreCount) {
                 bg_bar.getComponent(cc.Sprite).spriteFrame = this.bg_bar_win;
-                player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = ('/' + Math.abs((user.scoreCount)));
+                if (0 == user.scoreCount) {
+                    player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = (Math.abs(user.scoreCount));
+                } else {
+                    player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = ('/' + Math.abs(user.scoreCount).toFixed(2));
+                }
                 player.getChildByName("LabelAtlas_score_lose").getComponent(cc.Label).string = '';
             } else {
                 bg_bar.getComponent(cc.Sprite).spriteFrame = this.bg_bar_lose;
                 player.getChildByName("LabelAtlas_score_win").getComponent(cc.Label).string = '';
-                player.getChildByName("LabelAtlas_score_lose").getComponent(cc.Label).string = ('/' + Math.abs((user.scoreCount)));
+                player.getChildByName("LabelAtlas_score_lose").getComponent(cc.Label).string = ('/' + Math.abs(user.scoreCount).toFixed(2));
             }
         }
     },
